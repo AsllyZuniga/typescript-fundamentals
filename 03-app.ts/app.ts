cc60ca02cbb3e6e01ee55437f3aa80716aa90735
Aslly Zuñiga <asllyz2610@gmail.com>
2025-12-03 21:51:23 -0500

    feat(ts): add basic functions, default params, hero utilities and function type example

---
+++ 03-app.ts/app.ts
@@ -0,0 +1,45 @@
+// Basic Functions
+function add(a: number, b: number): number {
+  return a + b;
+}
+//
+// Counting heroes
+const countHeroes = (heroes: string[]): number => {
+  return heroes.length;
+};
+
+const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Green Lantern"];
+countHeroes(superHeroes);
+
+// Default parameters
+const callBatman = (call: boolean = false): void => {
+  if (call) {
+    console.log("Bat-signal activated");
+  }
+};
+
+callBatman();
+
+// Join heroes
+const joinHeroes = (people: string[]): string => {
+  return people.join(", ");
+};
+
+// Function type example
+const doNothing = (
+  numberValue: number,
+  text: string,
+  bool: boolean,
+  array: unknown[]
+): void => {};
+
+// Create a function type that matches "doNothing"
+type DoNothingType = (
+  numberValue: number,
+  text: string,
+  bool: boolean,
+  array: unknown[]
+) => void;
+
+let alsoDoesNothing: DoNothingType;
+alsoDoesNothing = doNothing;
